6fdc7b70d932640b10dc07e85473de693d753ea7
Mateusz Jemielity <M.Jemielity@chipcraft-ic.com>
2021-07-21 14:58:59 +0200

    [CHIPCRAFT][GCC][BUILDSYSTEM] Fix problems in canadian-cross-compilation.
    Issue existed for a long time, most likely caused by something borked in
    host environment. Tracker: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100017
    Partial patch is available, was taken into this commit.

--- libstdc++-v3/include/c_compatibility/fenv.h
+++ libstdc++-v3/include/c_compatibility/fenv.h
@@ -26,6 +26,10 @@
  *  This is a Standard C++ Library header.
  */
 
+#if !defined __cplusplus || defined _GLIBCXX_INCLUDE_NEXT_C_HEADERS
+# include_next <fenv.h>
+#else
+
 #ifndef _GLIBCXX_FENV_H
 #define _GLIBCXX_FENV_H 1
 
@@ -79,3 +83,6 @@ namespace std
 #endif // C++11
 
 #endif // _GLIBCXX_FENV_H
+
+#endif
+
